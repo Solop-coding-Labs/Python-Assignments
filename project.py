987266e8d5852d452626f3ee109ded285255bf46
Solop coding Labs <pomesl2507@gmail.com>
2025-08-22 08:32:59 +0200

    Add files via upload

---
+++ project.py
@@ -0,0 +1,12 @@
+#Story about Boy
+AGE='years old'
+gender='male'
+country='Zimbabwe'
+print('Boy is 18',AGE,'and is from',country)
+print('Boy is',gender)
+
+#Story about Girl
+X='lasagna'
+HER_AGE='21 years old'
+residency='Harare'
+print('Girl is',HER_AGE,'her favorite food is',X,'she stays in',residency)
